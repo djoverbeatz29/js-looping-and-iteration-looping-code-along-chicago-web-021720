d9a411dc0d5224a759cc02d70ba68410bdcd53b5
jequezinho <jroverby92@gmail.com>
2020-03-29 23:26:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let arr = [];
+    for (const name of names) {
+        arr.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return arr;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
